6333fb0dd5d6abddcba218260919d7a16f4425de
novice0840 <magnolia0840@gmail.com>
2024-12-22 00:19:25 +0900

    feat: react-vite-js-styledComponents

--- templates/react-vite-js-styledComponents/src/App.jsx
+++ templates/react-vite-js-styledComponents/src/App.jsx
@@ -2,6 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled, { css } from 'styled-components'
+
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: #BF4F74;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props => props.$primary && css`
+    background: #BF4F74;
+    color: white;
+  `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +48,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <Button>Normal Button</Button>
+        <Button $primary>Primary Button</Button>
     </>
   )
 }
